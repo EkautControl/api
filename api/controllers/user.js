a2b2bfeb9665ecf8eb8cbd192c5acedefe7fa656
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-18 20:20:18 -0300

    create update user info route

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -27,5 +27,29 @@ module.exports = () => {
     }
   };
 
+  controller.updateUser = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const userInfo = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+        notificationType: req.body.notificationType,
+      };
+
+      const result = await User.replaceOne({ _id: userId }, {
+        name: userInfo.name,
+        email: userInfo.email,
+        password: userInfo.password,
+        phone: userInfo.phone,
+        notificationType: userInfo.notificationType,
+      });
+      res.status(200).send(result);
+    } catch (err) {
+      res.status(500).send({ error: err.message });
+    }
+  };
+
   return controller;
 };
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,4 +3,5 @@ const controller = require('../controllers/user')();
 module.exports = (app) => {
   app.route('/api/user').post(controller.addUser);
   app.route('/api/user/:id').get(controller.getUser);
+  app.route('/api/user/:id').put(controller.updateUser);
 };
